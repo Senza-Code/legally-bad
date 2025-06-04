52708c8ee1f832fa5e4fd6725ee55cdd6ac76df4
Pearl Sikepe <pearl.sikepe@baruch.cuny.edu>
2025-06-04 14:28:18 -0400

    Add evil start button and chaos popup

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.addEventListener('mousemove', (e) => {
   fakeCursor.style.left = `${badX}px`;
   fakeCursor.style.top = `${badY}px`;
 });
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("mousemove", () => {
+  const maxX = window.innerWidth - startBtn.offsetWidth;
+  const maxY = window.innerHeight - startBtn.offsetHeight;
+
+  const randX = Math.floor(Math.random() * maxX);
+  const randY = Math.floor(Math.random() * maxY);
+
+  startBtn.style.position = "absolute";
+  startBtn.style.left = `${randX}px`;
+  startBtn.style.top = `${randY}px`;
+});
+
+setInterval(() => {
+  alert("Nope. 😈");
+}, 15000); // every 15 seconds
